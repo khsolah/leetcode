a19933f74873bb54f4db9ff1473c1b1b55617a9a
James Wu <james_wu@appdet.com>
2021-03-06 21:36:18 +0800

    #1 done | 72ms / 98.81%

--- #1/index.ts
+++ #1/index.ts
@@ -1,1 +1,25 @@
-console.log('#1')
+// 1. Two Sum
+// url: https://leetcode.com/problems/two-sum/
+import chalk from 'chalk'
+
+console.group(chalk.magenta('#1'))
+
+function twoSum(nums: number[], target: number): number[] {
+  let map: Map<number, number> = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.get(target - nums[i]) !== undefined) {
+      return [map.get(target - nums[i]) as number, i]
+    } else {
+      map.set(nums[i], i)
+    }
+  }
+
+  return []
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([3, 2, 4], 6))
+console.log(twoSum([3, 3], 6))
+
+console.groupEnd()
