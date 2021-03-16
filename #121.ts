6c1ae38b6b0e5b338b20902e9a389b87e6960c91
James Wu <james_wu@appdet.com>
2021-03-16 22:43:33 +0800

    #121 done | 104ms / 61.64%

--- #121.ts
+++ #121.ts
@@ -1,18 +1,16 @@
 function maxProfit(prices: number[]): number {
   const len = prices.length
-  let min, max, result = 0
+  let min = prices[0], result = 0
 
-  min = max = prices[0]
   for (let i = 1; i < len; i++) {
     if (min > prices[i]) {
-      result = Math.max(max - min, result)
-      min = max = prices[i]
-    } else if (prices[i] > max) {
-      max = prices[i]
+      min = prices[i]
+    } else if (prices[i] - min > result) {
+      result = prices[i] - min
     }
   }
 
-  return Math.max(result, max - min)
+  return result
 }
 
 console.log(maxProfit([5,3,1]))
